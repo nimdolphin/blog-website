ed3d613c2ac0699717167bb590c577c14365714a
Rimma <nimdolphin94@gmail.com>
2024-02-20 21:41:49 +0300

    added button for an article

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,11 +1,13 @@
 import AuthorInfo from "../components/AuthorInfo";
 import PostsList from "../components/PostsList";
 import Footer from "../components/Footer";
+import Button from "../components/Button";
 
 const PostsPage = () => {
   return (
     <>
       <AuthorInfo />
+      <Button type="submit" label="Write an article" width={500} />
       <PostsList />
       <Footer />
     </>
